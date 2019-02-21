273768e071f022d1e6330419856a94c9d54fea42
harmoniemand <harmoniemand@googlemail.com>
2019-02-21 15:07:51 +0100

    single post mit author

--- single.php
+++ single.php
@@ -9,6 +9,15 @@
 <?php endif; ?>    
 
 <div class="container mt-5 pb-5">
+    <div class="row">
+        <div class="col text-dark">
+            verfasst, am 
+            <?php the_date() ?>
+            von
+            <?php the_author_meta('display_name') ?>
+        </div>
+    </div>
+
     <div class="row">   
         <div class="col">
 
@@ -18,6 +27,22 @@
                 </p>
                 
                 
+        </div>
+    </div>
+
+    <div class="row mt-5 border border-bottom-0 bg-white">
+        <div class="col-1 p-3 ms-author-avatar">
+            <?php echo get_avatar(get_the_author_meta('ID'), 512)  ?>
+        </div>
+        <div class="col p-3 text-dark">
+            <h5 class="text-uppercase" style="font-size: 16px;">Über den Autor</h5>
+            <h4 class=""><?php the_author_meta('display_name') ?></h4>
+        </div>
+    </div>
+    <div class="row  border border-top-0 bg-white">
+        <div class="col-1 p-0 ms-author-avatar"></div>
+        <div class="col p-3 text-dark">
+            <p><?php the_author_meta('description') ?></p>
         </div>
     </div>
 </div>
