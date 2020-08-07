efe0925d47195ca4e3cd50828c7ce4f2eaa7f8ff
Jonathan Günz <jonathan.guenz@experimenta.science>
2020-08-08 00:15:06 +0200

    add moar space

--- page-kontaktdaten-erfassen.php
+++ page-kontaktdaten-erfassen.php
@@ -32,23 +32,26 @@
 
 <body>
 
-<?php while (have_posts()) : the_post(); ?>
+    <div style="height: 100vh;">
+        <?php while (have_posts()) : the_post(); ?>
 
 
-    <div class="container mt-5 pb-5">
-        <div class="row">
-            <div class="col wp-post">
+            <div class="container mt-5 pb-5">
+                <div class="row">
+                    <div class="col wp-post">
 
-                <h1><?php the_title() ?></h1>
-                <p>
-                    <?php the_content(); ?>
-                </p>
+                        <h1><?php the_title() ?></h1>
+                        <p>
+                            <?php the_content(); ?>
+                        </p>
 
 
+                    </div>
+                </div>
             </div>
-        </div>
+
+        <?php endwhile; ?>
     </div>
 
-<?php endwhile; ?>
 
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
