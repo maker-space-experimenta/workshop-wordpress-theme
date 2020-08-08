d31a0821a4dd91c5846a50984c3a2d5081052cb1
Jonathan Günz <jonathan.guenz@experimenta.science>
2020-08-08 03:14:07 +0200

    fix logged in users in foyer

--- page-ds-corona-anwesende.php
+++ page-ds-corona-anwesende.php
@@ -38,23 +38,50 @@
 
     global $wpdb;
 
-    $sql = "SELECT COUNT(mpl_id) as count FROM makerspace_presence_logs WHERE mpl_datetime BETWEEN %s AND %s GROUP BY mpl_user_id";
-    $day_start = (get_datetime()->setTime(0, 0, 0));
-    $day_end = (get_datetime()->setTime(23, 59, 59));
+    global $wpdb;
 
+        $logged_in_sql = "
+        SELECT count(*) as count FROM (
+            SELECT 
+                mpl_user_id,
+                MOD(COUNT(mpl_user_id), 2) as log_count,
+                MIN(mpl_datetime) as arrived_at,
+                MAX(mpl_datetime) as leaved_at
+            FROM `makerspace_presence_logs`
+            WHERE 
+                mpl_datetime between %s AND %s AND mpl_temp_visitor_id IS NULL
+            GROUP BY mpl_user_id
+            ) as tmp
+            WHERE log_count > 0
+        ";
 
-    $entries = $wpdb->get_results($wpdb->prepare(
-        $sql,
-        $day_start->format("Y-m-d H:i:s"),
-        $day_end->format("Y-m-d H:i:s")
-    ));
+        $logged_in_count = $wpdb->get_var($wpdb->prepare(
+            $logged_in_sql,
+            $from->format("Y-m-d H:i:s"),
+            $to->format("Y-m-d H:i:s")
+        ));
 
-    $count = 0;
-    foreach ($entries as $e) {
-        if ($e->count % 2 == 1) {
-            $count++;
-        }
-    }
+        $logged_in_sql = "
+        SELECT count(*) as count FROM (
+            SELECT 
+                mpl_temp_visitor_id,
+                MOD(COUNT(mpl_temp_visitor_id), 2) as log_count,
+                MIN(mpl_datetime) as arrived_at,
+                MAX(mpl_datetime) as leaved_at
+            FROM `makerspace_presence_logs`
+            WHERE 
+                mpl_datetime between %s AND %s AND mpl_temp_visitor_id IS NOT NULL
+            GROUP BY mpl_temp_visitor_id
+            ) as tmp
+            WHERE log_count > 0
+        ";
+
+        $logged_in_count += $wpdb->get_var($wpdb->prepare(
+            $logged_in_sql,
+            $from->format("Y-m-d H:i:s"),
+            $to->format("Y-m-d H:i:s")
+        ));
+
 
 
     ?>
@@ -71,7 +98,7 @@
         </div>
 
         <div class="" style="font-size: 150px; line-height: 200px;">
-            <span><?php echo $count ?></span>
+            <span><?php echo $logged_in_count ?></span>
             <span>Personen</span>
         </div>
 
